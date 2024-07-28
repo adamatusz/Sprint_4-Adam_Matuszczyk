041afdc52c99f79001ee8db53d9059ced32a7faf
adamatusz <adamatusz@gmail.com>
2024-07-28 17:43:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const inputText = document.getElementById("inputField");
+const submitButton = document.getElementById("clickButton");
+const listTasks = document.getElementById("listTasks");
+
+submitButton.addEventListener("click", () => {
+  if (inputText.value) {
+    const newTask = document.createElement("li");
+    const newTextTask = document.createElement("input");
+    newTextTask.setAttribute("class", "addedTask form-group control-label")
+    newTextTask.value = inputText.value;
+    newTextTask.disabled = true;
+    newTask.append(newTextTask);
+    createButton(newTask);
+    listTasks.prepend(newTask);
+    inputText.value = "";
+  } else {
+    alert("Nazwa zadania nie może być pusta.");
+  }
+});
+
+const createButton = (parent) => {
+  let editButton = document.createElement("button");
+  editButton.textContent = "Edytuj";
+  editButton.setAttribute("class", "btn-edit btn-info btn-outline-secondary");
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Usuń";
+  deleteButton.setAttribute("class", "btn-delete btn-danger");
+
+//   let createBr = document.createElement("hr");
+//   console.log(createBr)
+
+  parent.append(editButton);
+  parent.append(deleteButton);
+//   parent.append(createBr);
+
+
+  editButton.addEventListener("click", () => {
+    switch (editButton.textContent) {
+      case "Edytuj":
+        editButton.textContent = "Zatwierdź zmiany";
+        parent.firstChild.disabled = false;
+        break;
+      case "Zatwierdź zmiany":
+        if (parent.firstChild.value) {
+          editButton.textContent = "Edytuj";
+          parent.firstChild.disabled = true;
+        } else {
+          alert("To pole nie może być puste, ewentualnie usuń.");
+        }
+        break;
+      default:
+        alert("Unexpected button state.");
+    }
+  });
+
+  deleteButton.addEventListener("click", () => {
+    parent.remove();
+  });
+};
